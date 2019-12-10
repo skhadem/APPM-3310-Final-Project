f48a3e4f2cf8dad497c5010e3d3d14e2de6a0ca2
skhadem <soroush13khadem@gmail.com>
2019-12-10 12:50:26 -0700

    Fixed outlier, added some fun points

--- create_point_sets.m
+++ create_point_sets.m
@@ -1,8 +1,13 @@
-function [ptset1,ptset2,R,shift] = create_point_sets(num_points, with_noise, ...
+function [ptset1,ptset2,R,shift] = create_point_sets(prev_points, num_points, with_noise, ...
                                     max_val, max_shift, num_outliers)
 %CREATE_POINT_SETS Generate random points and transform them
 %   Detailed explanation goes here
-ptset1 = rand(num_points, 2)*(max_val);
+if (isempty(prev_points))
+    ptset1 = rand(num_points, 2)*(max_val);
+else
+    ptset1 = prev_points;
+    num_points = size(ptset1, 1);
+end
 center = mean(ptset1);
 
 % random shift
@@ -21,7 +26,7 @@ end
 if (num_outliers > 0)
     outlier_indexes = randi([1, num_points], 1, num_outliers);
     for ii = 1:length(outlier_indexes)
-        ptset2(ii, :) = (ptset2(ii, :)) + 20;
+        ptset2(outlier_indexes(ii), :) = (ptset2(outlier_indexes(ii), :)) + 20;
     end
 end
 
--- point_registration_appm3310.m
+++ point_registration_appm3310.m
@@ -9,8 +9,11 @@
 close all; clc;
 %%
 % generate k random points
-[map, pts, rot, shift] = create_point_sets(20, 1, ...
-                                    10, 5, 2);
+smiley_pts = load('smiley_points.mat');
+smiley_pts = smiley_pts.smiley_pts;
+% smiley_pts = [];
+[map, pts, rot, shift] = create_point_sets(smiley_pts, 20, 1, ...
+                                    10, 5, 0);
 figure;
 plot(map(:,1), map(:,2), 'ko');
 hold;
@@ -23,7 +26,7 @@ grid;
 x_range = xlim;
 y_range = ylim;
 
-%% SVD registration
+%% PCA registration
 centroid_map = mean(map);
 centroid_pts = mean(pts);
 
@@ -33,29 +36,54 @@ pts_n = pts - centroid_pts;
 covariance_mat = map_n'*pts;
 
 [U,S,V] = svd(covariance_mat);
-R = V*U';
+pca_rot = V*U';
 
-if (det(R)) < 0
-    V(:,3) = V(:,3)*-1;
-    R = V*U';
+if (det(pca_rot)) < 0
+    V(:,size(V,1)) = V(:,size(V,1))*-1;
+    pca_rot = V*U';
 end
 
-t = -R*centroid_map' + centroid_pts';
-t = t';
+pca_trans = -pca_rot*centroid_map' + centroid_pts';
+pca_trans = pca_trans';
 
-new_pts = R\(pts - t)';
+new_pts = pca_rot\(pts - pca_trans)';
 new_pts = new_pts';
 
 figure;
 plot(map(:,1), map(:,2), 'ko');
 hold;
 plot(new_pts(:,1), new_pts(:,2), 'rx');
-title('SVD Registration');
+title('PCA Registration');
 legend('Target Points', 'Data Points');
 xlim(x_range);
 ylim(y_range);
 
 grid;
 %% run KC algorithm
+% very interesting, the KC algorithm sometimes fails with the smiley
+% points. It seems to be a flip issue but I can't figure out where.
+kc_results = KCReg(map, pts, 2, 1);
+kc_angle = -results(3);
+kc_trans = -[results(1), results(2)];
+kc_rot = [cos(kc_angle) -sin(kc_angle); sin(kc_angle) cos(kc_angle)];
 
-KCReg(map, pts, 2, 1);
+
+
+%% run ICP. Note that the points are assumed to be 3D (on a plane)
+% This doesn't work
+% map3d = [map, ones(1,size(map,1))'];
+% pts3d = [pts, ones(1,size(pts,1))'];
+% 
+% [s, pca_rot, pca_trans, err, newP] = icp(map3d', pts3d');
+% new_pts_icp = newP';
+% 
+% figure;
+% plot(map3d(:,1), map3d(:,2), 'ko');
+% hold;
+% plot(new_pts_icp(:,1), new_pts_icp(:,2), 'rx');
+% title('ICP Registration');
+% legend('Target Points', 'Data Points');
+% xlim(x_range);
+% ylim(y_range);
+% grid;
+% 
